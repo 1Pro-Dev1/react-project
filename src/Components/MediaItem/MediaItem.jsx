cb9fb74bc08099c27e63009e8ef588762a73f924
1Pro-Dev1 <dude.alt.mail159@gmail.com>
2023-08-01 17:00:04 +0400

    Add/update feature XYZ

--- src/Components/MediaItem/MediaItem.jsx
+++ src/Components/MediaItem/MediaItem.jsx
@@ -7,7 +7,7 @@ export default function MediaItem({name, poster, vote, type, id}) {
     <div className="col-md-2 mb-2">
       <Link to={`/media/${type}/${id}`} className='decoration-none'>
         <div className="position-relative">
-          <img className='w-100' src={poster?`https:image.tmdb.org/t/p/w500${poster}`:`https://cdn.discordapp.com/attachments/1095603862871740418/1133817660686225510/person.jpg`}  alt="" />
+          <img className='w-100' src={poster?`https://image.tmdb.org/t/p/w500${poster}`:`https://cdn.discordapp.com/attachments/1095603862871740418/1133817660686225510/person.jpg`}  alt="" />
           {vote?<p className='text-white position-absolute top-0 end-0 bg-primary h6 py-2 px-2'>{vote}</p>:""}
           <h6 className='text-white my-2 mt-0'>{name}</h6>
         </div>
