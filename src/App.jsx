0a6ef3bf2270e4f3ba662946557abe3f570f7c1f
1Pro-Dev1 <dude.alt.mail159@gmail.com>
2023-08-02 20:01:54 +0400

    Some Edits

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import Layout from './Components/Layout/Layout';
 import Home from "./Components/Home/Home"
 import TV from "./Components/TV/TV"
 import Movies from "./Components/Movies/Movies"
-import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Notfound from './Components/Notfound/Notfound';
 import MediaPage from './MediaPage/MediaPage';
 
-let routers = createHashRouter([
+let routers = createBrowserRouter([
     {path:"/", element:<Layout/>, children:[
         {index:true, element:<Home/>},
         {path:"movies", element:<Movies/>},
